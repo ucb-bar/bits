82b14e6f4dce0f26f16e2d44ab8a830156a25887
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2015-03-17 19:01:00 -0700

    New mechanism to specify options. Benchmarks are killed at the end now

--- bits-manager/benchmarks.py
+++ bits-manager/benchmarks.py
@@ -9,9 +9,15 @@ from utils import make_dir
 
 def build_cl_options(workload):
     # each option has 2 parts
-    first = workload['cl-options'][0]
-    second = workload['cl-options'][1]
-    return [first, "\"" + second + "\""]
+
+    result = []
+
+    for option in workload['cl-options']:
+        first = option[0]
+        second = option[1]
+        result += [first, second]
+#result += [first, "\"" + second + "\""]
+    return result
 
 def run_benchmarks(benchmarks, workloads):
 
@@ -36,8 +42,10 @@ def run_benchmarks(benchmarks, workloads):
         # setup all workloads
         for workload in benchmark['workloads']:
 
+            cl_options = build_cl_options(workloads[workload])
             path = workloads[workload]['path']
             run_cmd = ["/usr/bin/python", path, "setup"]
+            run_cmd += cl_options
 
             print "Setting up workload: " + ' '.join(run_cmd)
             subprocess.call(run_cmd, stdout = fout, stderr = ferr)
@@ -63,3 +71,17 @@ def run_benchmarks(benchmarks, workloads):
         # if is concurrent we need to wait for all workloads to finish
         for p in process_handles:
             p.wait()
+
+        # exit workloads
+        for workload in benchmark['workloads']:
+            print "killing workload: " + workload
+
+            # build command line options
+            cl_options = build_cl_options(workloads[workload])
+            
+            run_cmd = ["/usr/bin/python", path, "stop"]
+            run_cmd += cl_options
+
+            subprocess.call(run_cmd, stdout = fout, stderr = ferr)
+
+
